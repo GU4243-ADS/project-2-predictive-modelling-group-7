049dcb886a67ae1724651407e051c10e96fac8ff
nas2184 <nas2184@columbia.edu>
2018-02-26 15:18:23 -0500

    xgboost

--- lib/xgboost.r
+++ lib/xgboost.r
@@ -2,7 +2,7 @@
 ### Train a classification model with training images ###
 #########################################################
 
-### Author: Yuting Ma
+### Author: Nicole
 ### Project 3
 ### ADS Spring 2016
 
@@ -17,21 +17,43 @@ train <- function(dat_train, label_train, par = NULL){
   ### Output: training model specification
   
   ### load libraries
-  library("gbm")
+  library(xgboost)
   
   ### Train with gradient boosting model
+  # if(is.null(par)){
+  #   depth <- 3
+  #  } else {
+  #   depth <- par$depth
+  # }
+  # fit_gbm <- gbm.fit(x = dat_train, y = label_train,
+  #                   n.trees = 2000,
+  #                   distribution = "bernoulli",
+  #                   interaction.depth = depth,
+  #                   bag.fraction = 0.5,
+  #                   verbose = FALSE)
+  # best_iter <- gbm.perf(fit_gbm, method = "OOB", plot.it = FALSE)
+  
+  # return(list(fit = fit_gbm, iter = best_iter))
+  #}
+  
+  
+  ### Train with XGboost
   if(is.null(par)){
     depth <- 3
   } else {
     depth <- par$depth
   }
-  fit_gbm <- gbm.fit(x = dat_train, y = label_train,
-                     n.trees = 2000,
-                     distribution = "bernoulli",
-                     interaction.depth = depth, 
-                     bag.fraction = 0.5,
-                     verbose = FALSE)
-  best_iter <- gbm.perf(fit_gbm, method = "OOB", plot.it = FALSE)
-
-  return(list(fit = fit_gbm, iter = best_iter))
+  
+  bst <- xgboost(data = dat_train, label = label_train,
+                 max.depth = 2,
+                 eta = .2,
+                 nround = 2,
+                 nthread = 2, objective = "binary:logistic")
+  
+  test.error <- sum(predict(bst, data.matrix(test[,-1])) != test[-1]/lenght(test[,1]))
 }
+  
+# max.depth: depth of tree
+# eta: step size shrinkage used to prevent overfitting; value between 0 and 1 (default 0.3)
+# nround: number of trees
+# nthread: number of CPU threads to use; can improve efficiency (no default)
