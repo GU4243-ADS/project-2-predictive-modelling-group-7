3f9147b5ba4a670f4478c33889fa8be8028614c1
nas2184 <nas2184@columbia.edu>
2018-03-04 21:25:47 -0500

    more updates

--- lib/train.R
+++ lib/train.R
@@ -7,31 +7,30 @@
 ### ADS Spring 2018
 
 
-train <- function(dat_train, label_train, par = NULL){
+gbm_train <- function(dat_train, label_train){
+  library(gbm)
   
-  ### Train a Gradient Boosting Model (GBM) using processed features from training images
-  
-  ### Input: 
-  ###  -  processed features from images 
-  ###  -  class labels for training images
-  ### Output: training model specification
-  
-  ### load libraries
-  library("gbm")
-  
-  ### Train with gradient boosting model
-  if(is.null(par)){
-    depth <- 3
-  } else {
-    depth <- par$depth
-  }
   fit_gbm <- gbm.fit(x = dat_train, y = label_train,
                      n.trees = 2000,
                      distribution = "bernoulli",
-                     interaction.depth = depth, 
+                     interaction.depth = 3,
                      bag.fraction = 0.5,
                      verbose = FALSE)
   best_iter <- gbm.perf(fit_gbm, method = "OOB", plot.it = FALSE)
 
   return(list(fit = fit_gbm, iter = best_iter))
+}
+
+
+xgb_train <- function(dat_train, label_train){
+    
+    library(xgboost)
+    
+    fit_xgb <- xgboost(data = dat_train, label = label_train,
+                        max.depth = 3,
+                        eta = 0.5,
+                        nround = 20,
+                        nthread = 2,
+                        objective = "binary:logistic")
+   
 }
